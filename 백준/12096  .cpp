b7db07cde5a04882f9d366f83fd7d6d9c7af8e15
mwngh <mwngh@naver.com>
2018-07-30 22:31:23 +0900

    넌센스

---
+++ 백준/12096  .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "02-521-0487" << endl;
+
+	return 0;
+}
+
+/*
+	진짜 특이한 문제
+	F12로 문제, 풀이, 힌트를 보면 주석으로 Base64로 인코딩된 문자열이 있다.
+	그 문자를 디코딩해보면 https://startlink.io/ 의 전화번호를 출력하게 되있다.
+*/
