84add7e1fb85a128c43104d5e80c561eab66025e
RunaticMoon <39324508+RunaticMoon@users.noreply.github.com>
2019-01-07 16:38:31 +0900

    Greedy

--- 백준/1449 수리공 항승.cpp
+++ 백준/1449 수리공 항승.cpp
@@ -35,3 +35,41 @@ int main() {
 
 	return 0;
 }
+
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+	int N, L, temp, count;
+	vector<int> arr;
+
+	cin >> N >> L;
+
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		arr.push_back(temp);
+	}
+
+	sort(arr.begin(), arr.end());
+	arr.push_back(INT_MAX);
+
+	count = 0;
+	for (int i = 0; i < arr.size() - 1; i++) {
+		for (int j = i + 1; j < arr.size(); j++) {
+			if (arr[j] - arr[i] >= L) {
+				count++;
+				i = j - 1;
+				break;
+			}
+		}
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
+*/
