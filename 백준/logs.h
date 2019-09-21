4134d5a5031746a7d1ea969da34857b94148115f
bean <mwngh3@gmail.com>
2019-09-22 00:46:44 +0900

    로그용 헤더 수정

--- 백준/logs.h
+++ 백준/logs.h
@@ -13,24 +13,24 @@
 
 #define L(Variable) (#Variable),(Variable)
 
-static unsigned long long __PRINT_INDEX__ = 1;
+static bool __PRINT_INDEX__ = true;
 
 template<typename T>
 void print(T arg) {
 	std::cout << arg << std::endl;
-	__PRINT_INDEX__++;
+	__PRINT_INDEX__ = !__PRINT_INDEX__;
 }
 
 template<typename T>
 void printType(T arg) {
 	std::cout << arg;
-	if (__PRINT_INDEX__ % 2) {
-		std::cout << " : ";
+	if (__PRINT_INDEX__) {
+		std::cout << ": ";
 	}
 	else {
 		std::cout << ", ";
 	}
-	__PRINT_INDEX__++;
+	__PRINT_INDEX__ = !__PRINT_INDEX__;
 }
 
 template<typename T, typename... Args>
