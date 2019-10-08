743d703bb56e2a60ba8c9bb1192fa5b2b2c7d823
bean <mwngh3@gmail.com>
2019-10-09 01:54:57 +0900

    기초

---
+++ 백준/10951 A+B - 4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int A, B;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+		cin >> A >> B;
+		if (cin.eof()) break;
+
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
