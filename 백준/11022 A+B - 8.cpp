a502973c26ab2bbc73db5fa49d9f4d900a863f77
bean <mwngh3@gmail.com>
2019-10-09 01:50:22 +0900

    기초

---
+++ 백준/11022 A+B - 8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int T;
+int A, B;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	for (int test_case = 1; test_case <= T; test_case++) {
+		cin >> A >> B;
+		cout << "Case #" << test_case << ": "
+			<< A << " + " << B << " = " << A + B << "\n";
+	}
+
+	return 0;
+}
