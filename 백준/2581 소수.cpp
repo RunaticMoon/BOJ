e60050a7b7107cc71883ade54ec8785ac9a4e965
bean <mwngh3@gmail.com>
2019-10-28 15:38:49 +0900

    수학

---
+++ 백준/2581 소수.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX = 1e4 + 1;
+
+int N, M, minN, sum;
+bool erathos[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	memset(erathos, true, sizeof(erathos));
+	erathos[0] = false;
+	erathos[1] = false;
+	for (int i = 2; i < MAX; i++) {
+		if (erathos[i]) {
+			for (int j = 2; i*j < MAX; j++) {
+				erathos[i*j] = false;
+			}
+		}
+	}
+
+	cin >> M >> N;
+	for (int i = M; i <= N; i++) {
+		if (erathos[i]) {
+			if (minN == 0) minN = i;
+			sum += i;
+		}
+	}
+
+	if (sum == 0)
+		cout << -1;
+	else
+		cout << sum << '\n' << minN;
+
+	return 0;
+}
