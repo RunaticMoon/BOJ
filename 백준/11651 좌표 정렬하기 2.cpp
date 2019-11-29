4e4af4dd228585e74e3829ef7a2426244ff8b822
bean <mwngh3@gmail.com>
2019-11-29 16:44:39 +0900

    Sorting

---
+++ 백준/11651 좌표 정렬하기 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int MAX = 1e5 + 1;
+
+int N;
+pii arr[MAX];
+
+bool comp(const pii& a, const pii& b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+	else return a.second < b.second;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i].first >> arr[i].second;
+	}
+
+	sort(arr, arr + N, comp);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].first << ' ' << arr[i].second << '\n';
+	}
+
+	return 0;
+}
